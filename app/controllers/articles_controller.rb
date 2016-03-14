8f9e2058625a591150e48fe1fafecb6a0e04f383
affimintus <affimintus@gmail.com>
2016-03-14 12:45:58 +0530

    indentation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,36 +17,33 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    
     @article = Article.new(article_params)
-    
     if @article.save
-    redirect_to @article
+      redirect_to @article
     else
       render 'new'
-     end
-end
-
-def update
-  @article = Article.find(params[:id])
- 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+    end
   end
-end
- 
- def destroy
- @article = Article.find(params[:id])
- @article.destroy
+    
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+   
+   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path 
+  end
 
-redirect_to articles_path 
-end
- 
+   
   private 
   def article_params
-      params.require(:article).permit(:title, :text)
-    
-     end
+    params.require(:article).permit(:title, :text)
+  end
 end
